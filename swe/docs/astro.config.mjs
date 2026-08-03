ef26b7a0b0bf48b8db084d4d582c95bd17e7acc6
Machado dos Santos <29000264+msmachadodossantos@users.noreply.github.com>
2026-08-03 18:36:53 +0100

    Update astro.config.mjs

--- swe/docs/astro.config.mjs
+++ swe/docs/astro.config.mjs
@@ -14,30 +14,134 @@ export default defineConfig({
       autoTheme: true,   // Alterna automaticamente entre claro/escuro com base no Starlight
     }),
     starlight({
-      title: 'ASANOBA Docs',
+      title: 'ASANOBA [0.0.0]',
       sidebar: [
         {
-          label: 'DB',
+          label: 'Getting Started',
+          collapsed: true,
           items: [
-            { autogenerate: { directory: 'db' } },
+            { autogenerate: { directory: 'getting-started' } },
           ],
         },
         {
-          label: 'DEV',
+          label: 'Project Documentation',
+          collapsed: true,
           items: [
-            { autogenerate: { directory: 'dev' } }
+            {
+              label: 'Requirements Engineering',
+              collapsed: true,
+              items: [
+                { autogenerate: { directory: 'project-documentation/requirements-engineering' } },
+              ],
+            },
+            {
+              label: 'Design & Architecture',
+              collapsed: true,
+              items: [
+                { autogenerate: { directory: 'project-documentation/design-and-architecture' } },
+              ],
+            },
+            {
+              label: 'Project & Quality Management',
+              collapsed: true,
+              items: [
+                { autogenerate: { directory: 'project-documentation/project-and-quality-management' } },
+              ],
+            },
+            {
+              label: 'Testing & QA',
+              collapsed: true,
+              items: [
+                { autogenerate: { directory: 'project-documentation/testing-and-qa' } },
+              ],
+            },
+            {
+              label: 'Change & Release Management',
+              collapsed: true,
+              items: [
+                { autogenerate: { directory: 'project-documentation/change-and-release-management' } },
+              ],
+            },
           ],
         },
         {
-          label: 'SWE',
+          label: 'Internal Documentation',
+          collapsed: true,
           items: [
-            { autogenerate: { directory: 'swe' } },
-          ],
-        },
-        {
-          label: 'UXD',
-          items: [
-            { autogenerate: { directory: 'uxd' } },
+            {
+              label: 'Platform and Operations Teams',
+              collapsed: true,
+              items: [
+                {
+                  label: 'Infra Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/paot/infra-team' } },
+                  ],
+                },
+                {
+                  label: 'Operations Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/paot/operations-team' } },
+                  ],
+                },
+              ],
+            },
+            {
+              label: 'Product & Core Stream',
+              collapsed: true,
+              items: [
+                {
+                  label: 'Business Analysis Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/pacs/business-analysis-team' } },
+                  ],
+                },
+                {
+                  label: 'Java Dev Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/pacs/java-dev-team' } },
+                  ],
+                },
+                {
+                  label: 'PHP Dev Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/pacs/php-dev-team' } },
+                  ],
+                },
+                {
+                  label: 'Product Design Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/pacs/product-design-team' } },
+                  ],
+                },
+              ],
+            },
+            {
+              label: 'Qualification and Basic Engineering Teams',
+              collapsed: true,
+              items: [
+                {
+                  label: 'Core Engineering Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/qabet/core-engineering-team' } },
+                  ],
+                },
+                {
+                  label: 'Data Team',
+                  collapsed: true,
+                  items: [
+                    { autogenerate: { directory: 'internal-documentation/qabet/data-team' } },
+                  ],
+                },
+              ],
+            },
           ],
         },
       ],
